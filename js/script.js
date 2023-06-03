07034a25d71f702628eaa0b6016bfe2a7003f94a
Stepan0202 <stepan020293@gmail.com>
2023-06-04 01:06:12 +0300

    homework 4 is done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*
+TODO
+1. Separate function to checking digits quantity in number
+2. 
+*/
+
 // Дано два різні числа. Визначити, яке з них більше, а яке менше.
 let input1HM4Task1 = document.querySelector("#hw4t1in1");
 let input2HM4Task1 = document.querySelector("#hw4t1in2");
@@ -32,17 +38,17 @@ let submitHM4Task2 = document.querySelector("#hw4t2submit");
 submitHM4Task2.addEventListener('click', (e) => {
     e.preventDefault();
     let sign = document.querySelector(".sign_hw4_t2");
-    let firstNumber = parseFloat(input1HM4Task2.value);
-    let secondNumber = parseFloat(input2HM4Task2.value);
+    let firstNumber = parseFloat(input1HM4Task2.value)*1000;
+    let secondNumber = parseFloat(input2HM4Task2.value)*0.305;
    
     switch(true){
-        case firstNumber > secondNumber*0.305:
+        case firstNumber > secondNumber:
             sign.innerHTML = ">";
             break;
-        case firstNumber == secondNumber*0.305:
+        case firstNumber == secondNumber:
             sign.innerHTML = "=";
             break;
-        case firstNumber < secondNumber*0.305:
+        case firstNumber < secondNumber:
             sign.innerHTML = "<";
             break;
         default:
@@ -50,20 +56,145 @@ submitHM4Task2.addEventListener('click', (e) => {
     }
 });
 // Визначити, чи є число a дільником числа b? І навпаки. (Дати дві відповіді)
+let input1HW4Task3 = document.querySelector("#hw4t3in1");
+let input2HW4Task3 = document.querySelector("#hw4t3in2");
+let submitHW4Task3 = document.querySelector("#hw4t3submit");
+submitHW4Task3.addEventListener('click', (e) => {
+    e.preventDefault();
+    let firstNumber = parseFloat(input1HW4Task3.value);
+    let secondNumber = parseFloat(input2HW4Task3.value);
+    let aDivideb = document.querySelector(".hw4t3answer1");
+    let bDividea = document.querySelector(".hw4t3answer2");
 
+    aDivideb.innerHTML = `'a' is a divisor of 'b': <span class="fs-3">${secondNumber  % firstNumber == 0}</span>`;
+    bDividea.innerHTML = `'b' is a divisor of 'a': <span class="fs-3">${firstNumber % secondNumber == 0}</span>`;
+    console.log(firstNumber % secondNumber );
+    console.log(secondNumber % firstNumber);
+
+    
+});
 // Дано число. Визначити, чи закінчується воно парною цифрою чи непарною? Вивести останню цифру.
-
+let input1HW4Task4 = document.querySelector("#hw4t4in1");
+let input2HW4Task4 = document.querySelector("#hw4t4in2");
+let submitHW4Task4 = document.querySelector("#hw4t4submit");
+submitHW4Task4.addEventListener('click', (e) => {
+    e.preventDefault();
+    let firstNumber = parseFloat(input1HW4Task4.value);
+    let answer = document.querySelector(".hw4t4answer1");
+    let lastNumber;
+    let oddOrNot;
+    if(Math.abs(firstNumber) < 10 ) lastNumber = firstNumber;
+    else lastNumber = Math.abs(firstNumber % 10);
+    console.log(firstNumber < 10);
+    console.log(firstNumber % 10);
+    lastNumber % 2 == 0 ? oddOrNot = "even" : oddOrNot = "odd";
+    answer.innerHTML = `Last number is: <span class="fs-3">${lastNumber}</span>. It\`s   <span class="fs-3">${oddOrNot}</span>`;    
+});
 // Дано двозначне число. Визначити, яка з його цифр більша: перша чи друга?
-
+let input1HW4Task5 = document.querySelector("#hw4t5in1");
+let input2HW4Task5 = document.querySelector("#hw4t5in2");
+let submitHW4Task5 = document.querySelector("#hw4t5submit");
+submitHW4Task5.addEventListener('click', (e) => {
+    e.preventDefault();
+    let number = parseInt(input1HW4Task5.value);
+    if((number > -10 && number < 10) || (number < -99 || number > 99)){
+        alert("Number must be two-digit");
+        input1HW4Task5.value = "";
+        return false;
+    }
+    let firstNumber = parseInt(input1HW4Task5.value/10);
+    let secondNumber = parseInt(input1HW4Task5.value)%10;
+    let answer = document.querySelector(".hw4t5answer1");
+    switch(true){
+        case firstNumber < secondNumber:
+            answer.innerHTML = `Answer: ${firstNumber} < ${secondNumber}`;
+            break;
+        case firstNumber > secondNumber:
+            answer.innerHTML = `Answer: ${firstNumber} > ${secondNumber}`;
+            break;
+        case firstNumber == secondNumber:
+            answer.innerHTML = `Answer: ${firstNumber} = ${secondNumber}`;
+            break;
+    }
+      
+});
 /*Дано тризначне число.
     Визначити чи є парною сума його цифр.
     Визначити, чи кратна сума цифр п'яти.
     Визначити чи є добуток його цифр більше 100.*/
-
-
+    let input1HW4Task6 = document.querySelector("#hw4t6in1");
+    let input2HW4Task6 = document.querySelector("#hw4t6in2");
+    let submitHW4Task6 = document.querySelector("#hw4t6submit");
+    submitHW4Task6.addEventListener('click', (e) => {
+        e.preventDefault();
+        let number = parseInt(input1HW4Task6.value);
+        if((number > -100 && number < 100) || (number < -999 || number > 999)){
+            alert("Number must be three-digit");
+            input1HW4Task6.value = "";
+            return false;
+        }
+        let firstNumber = parseInt(input1HW4Task6.value/100);
+        let secondNumber = parseInt(input1HW4Task6.value%100/10);
+        let thirdNumber = parseInt(input1HW4Task6.value)%10;
+        let numbersSum = firstNumber+secondNumber+thirdNumber;
+        let numbersMultiply = firstNumber*secondNumber*thirdNumber;
+        let answer1 = document.querySelector(".hw4t6answer1");
+        let answer2 = document.querySelector(".hw4t6answer2");
+        let answer3 = document.querySelector(".hw4t6answer3");
+        answer1.innerHTML = numbersSum%2 == 0;
+        answer2.innerHTML = numbersSum%5 == 0;
+        answer3.innerHTML = numbersMultiply > 100;
+    });
 
 /*Дано тризначне число.
     Чи правда, що всі цифри однакові?
     Чи є серед цифр цифри однакові?*/
-
+    let input1HW4Task7 = document.querySelector("#hw4t7in1");
+    let input2HW4Task7 = document.querySelector("#hw4t7in2");
+    let submitHW4Task7 = document.querySelector("#hw4t7submit");
+    submitHW4Task7.addEventListener('click', (e) => {
+        e.preventDefault();
+        let number = parseInt(input1HW4Task7.value);
+        if((number > -100 && number < 100) || (number < -999 || number > 999)){
+            alert("Number must be three-digit");
+            input1HW4Task7.value = "";
+            return false;
+        }
+        let firstNumber = parseInt(input1HW4Task7.value/100);
+        let secondNumber = parseInt(input1HW4Task7.value%100/10);
+        let thirdNumber = parseInt(input1HW4Task7.value)%10;
+        let answer1 = document.querySelector(".hw4t7answer1");
+        let answer2 = document.querySelector(".hw4t7answer2");
+        answer1.innerHTML = (firstNumber == secondNumber && secondNumber == thirdNumber);
+        answer2.innerHTML = (firstNumber == secondNumber || secondNumber == thirdNumber || firstNumber == thirdNumber);
+        
+    });
 // Визначити, чи є задане шестизначне число дзеркальним? (123321, 147741)
+let input1HW4Task8 = document.querySelector("#hw4t8in1");
+let input2HW4Task8 = document.querySelector("#hw4t8in2");
+let submitHW4Task8 = document.querySelector("#hw4t8submit");
+submitHW4Task8.addEventListener('click', (e) => {
+    e.preventDefault();
+    let number = parseInt(input1HW4Task8.value);
+    if((number > -100000 && number < 100000) || (number < -999999 || number > 999999)){
+        alert("Number must be six-digit");
+        input1HW4Task8.value = "";
+        return false;
+    }
+    let digits = input1HW4Task8.value.split("");
+    let isMirror = true;
+    if(number < 0) digits.splice(0, 1);
+    let len = digits.length;
+    digits.forEach((digit, key) => {
+        digits[key] = parseInt(digit);
+    })
+    for(let i = 0; i < len/2; i++){
+        if(digits[i] != digits[len-1-i]){
+            isMirror = false;
+        }
+    }
+    let answer1 = document.querySelector(".hw4t8answer1");
+    answer1.innerHTML = isMirror;
+    
+    
+});
